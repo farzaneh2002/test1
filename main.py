381a6bd2a954434b2969a1c9b59026cf2465c66a
farzaneh2002 <moradifarzaneh813@gmail.com>
2025-04-30 14:47:50 +0000

    i do my exersises parham jooon

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
 print('hello parham khare')
+list1= ['parham','farzaneh']
+list2=['parham2', 'farzaneh2']
+print(list1, list2)
+print(len(list1))
+print(len(list2))
+print(list1 + list2)
+print(list1[0])
+list1[0] = 'parham3'
+print(list1)
+list1[0:1] = []
+print(list1)
+
+
+x=0
+while x<20 :
+    print(x)
+    x=x+1
